ee17e190484d581dbc8280ea2ea5a001642ccc91
Georgios Taskos <george@xplatsolutions.com>
2015-06-04 21:06:09 -0400

    Initial commit.

---
+++ PollfishXamarinPhone/ApiDefinition.cs
@@ -0,0 +1,225 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using ObjCRuntime;
+using CoreGraphics;
+//using pollfish;
+
+namespace PollfishXamarinPhone
+{
+	// The first step to creating a binding is to add your native library ("libNativeLibrary.a")
+	// to the project by right-clicking (or Control-clicking) the folder containing this source
+	// file and clicking "Add files..." and then simply select the native library (or libraries)
+	// that you want to bind.
+	//
+	// When you do that, you'll notice that MonoDevelop generates a code-behind file for each
+	// native library which will contain a [LinkWith] attribute. MonoDevelop auto-detects the
+	// architectures that the native library supports and fills in that information for you,
+	// however, it cannot auto-detect any Frameworks or other system libraries that the
+	// native library may depend on, so you'll need to fill in that information yourself.
+	//
+	// Once you've done that, you're ready to move on to binding the API...
+	//
+	//
+	// Here is where you'd define your API definition for the native Objective-C library.
+	//
+	// For example, to bind the following Objective-C class:
+	//
+	//     @interface Widget : NSObject {
+	//     }
+	//
+	// The C# binding would look like this:
+	//
+	//     [BaseType (typeof (NSObject))]
+	//     interface Widget {
+	//     }
+	//
+	// To bind Objective-C properties, such as:
+	//
+	//     @property (nonatomic, readwrite, assign) CGPoint center;
+	//
+	// You would add a property definition in the C# interface like so:
+	//
+	//     [Export ("center")]
+	//     CGPoint Center { get; set; }
+	//
+	// To bind an Objective-C method, such as:
+	//
+	//     -(void) doSomething:(NSObject *)object atIndex:(NSInteger)index;
+	//
+	// You would add a method definition to the C# interface like so:
+	//
+	//     [Export ("doSomething:atIndex:")]
+	//     void DoSomething (NSObject object, int index);
+	//
+	// Objective-C "constructors" such as:
+	//
+	//     -(id)initWithElmo:(ElmoMuppet *)elmo;
+	//
+	// Can be bound as:
+	//
+	//     [Export ("initWithElmo:")]
+	//     IntPtr Constructor (ElmoMuppet elmo);
+	//
+	// For more information, see http://developer.xamarin.com/guides/ios/advanced_topics/binding_objective-c/
+	//
+
+	// @interface UserAttributesDictionary : NSMutableDictionary
+	[BaseType (typeof(NSMutableDictionary))]
+	interface UserAttributesDictionary
+	{
+//		NSMutableDictionary _dict;
+
+		// -(id)initWithObjects:(const id *)objects forKeys:(const id<NSCopying> *)keys count:(NSUInteger)count;
+		[Export ("initWithObjects:forKeys:count:")]
+		IntPtr Constructor (NSObject[] objects, NSCopying[] keys, nuint count);
+
+		// -(id)objectForKey:(id)aKey;
+		[Export ("objectForKey:")]
+		NSObject ObjectForKey (NSObject aKey);
+
+		// -(NSEnumerator *)keyEnumerator;
+		[Export ("keyEnumerator")]
+//		[Verify (MethodToProperty)]
+		NSEnumerator KeyEnumerator { get; }
+
+		// -(void)setObject:(id)anObject forKey:(id)aKey;
+		[Export ("setObject:forKey:")]
+		void SetObject (NSObject anObject, NSObject aKey);
+
+		// -(void)removeObjectForKey:(id)aKey;
+		[Export ("removeObjectForKey:")]
+		void RemoveObjectForKey (NSObject aKey);
+
+		// -(NSUInteger)count;
+		[Export ("count")]
+//		[Verify (MethodToProperty)]
+		nuint Count { get; }
+
+		// -(void)dealloc;
+		[Export ("dealloc")]
+		void Dealloc ();
+
+		// -(UserAttributesDictionary *)setFacebookId:(NSString *)facebook_id;
+		[Export ("setFacebookId:")]
+		UserAttributesDictionary SetFacebookId (string facebook_id);
+
+		// -(UserAttributesDictionary *)setTwitterId:(NSString *)twitter_id;
+		[Export ("setTwitterId:")]
+		UserAttributesDictionary SetTwitterId (string twitter_id);
+
+		// -(UserAttributesDictionary *)setCustomAtributesWithKey:(NSString *)attr_key andAttrValue:(NSString *)attr_value;
+		[Export ("setCustomAtributesWithKey:andAttrValue:")]
+		UserAttributesDictionary SetCustomAtributesWithKey (string attr_key, string attr_value);
+
+		// -(UserAttributesDictionary *)setLinkedInId:(NSString *)linkedin_id;
+		[Export ("setLinkedInId:")]
+		UserAttributesDictionary SetLinkedInId (string linkedin_id);
+
+		// -(UserAttributesDictionary *)setGoogleId:(NSString *)google_id;
+		[Export ("setGoogleId:")]
+		UserAttributesDictionary SetGoogleId (string google_id);
+
+		// -(UserAttributesDictionary *)setEmail:(NSString *)email;
+		[Export ("setEmail:")]
+		UserAttributesDictionary SetEmail (string email);
+
+		// -(UserAttributesDictionary *)setPhone:(NSString *)phone;
+		[Export ("setPhone:")]
+		UserAttributesDictionary SetPhone (string phone);
+
+		// -(UserAttributesDictionary *)setName:(NSString *)name;
+		[Export ("setName:")]
+		UserAttributesDictionary SetName (string name);
+
+		// -(UserAttributesDictionary *)setSurname:(NSString *)surname;
+		[Export ("setSurname:")]
+		UserAttributesDictionary SetSurname (string surname);
+
+		// -(UserAttributesDictionary *)setAge:(NSString *)age;
+		[Export ("setAge:")]
+		UserAttributesDictionary SetAge (string age);
+
+		// -(UserAttributesDictionary *)setGender:(NSString *)gender;
+		[Export ("setGender:")]
+		UserAttributesDictionary SetGender (string gender);
+
+		// -(UserAttributesDictionary *)setAgeGroup:(NSString *)age_group;
+		[Export ("setAgeGroup:")]
+		UserAttributesDictionary SetAgeGroup (string age_group);
+
+		// -(UserAttributesDictionary *)setYearOfBirth:(NSString *)year_of_birth;
+		[Export ("setYearOfBirth:")]
+		UserAttributesDictionary SetYearOfBirth (string year_of_birth);
+
+		// -(UserAttributesDictionary *)setPlaceOfBirth:(NSString *)place_of_birth;
+		[Export ("setPlaceOfBirth:")]
+		UserAttributesDictionary SetPlaceOfBirth (string place_of_birth);
+
+		// -(UserAttributesDictionary *)setPlaceOfRecidence:(NSString *)place_of_residence;
+		[Export ("setPlaceOfRecidence:")]
+		UserAttributesDictionary SetPlaceOfRecidence (string place_of_residence);
+
+		// -(UserAttributesDictionary *)setNationality:(NSString *)nationality;
+		[Export ("setNationality:")]
+		UserAttributesDictionary SetNationality (string nationality);
+
+		// -(UserAttributesDictionary *)setMaritalStatus:(NSString *)marital_status;
+		[Export ("setMaritalStatus:")]
+		UserAttributesDictionary SetMaritalStatus (string marital_status);
+
+		// -(UserAttributesDictionary *)setReligion:(NSString *)religion;
+		[Export ("setReligion:")]
+		UserAttributesDictionary SetReligion (string religion);
+
+		// -(UserAttributesDictionary *)setPoliticalView:(NSString *)political_view;
+		[Export ("setPoliticalView:")]
+		UserAttributesDictionary SetPoliticalView (string political_view);
+
+		// -(UserAttributesDictionary *)setRace:(NSString *)race;
+		[Export ("setRace:")]
+		UserAttributesDictionary SetRace (string race);
+	}
+
+	// @interface Pollfish : NSObject
+	[BaseType (typeof(NSObject))]
+	interface Pollfish
+	{
+		// +(void)initAtPosition:(int)pos withPadding:(int)margin andDeveloperKey:(NSString *)key andDebuggable:(BOOL)isDebuggable andCustomMode:(BOOL)isCustomMode;
+		[Static]
+		[Export ("initAtPosition:withPadding:andDeveloperKey:andDebuggable:andCustomMode:")]
+		void InitAtPosition (int pos, int margin, string key, bool isDebuggable, bool isCustomMode);
+
+		// +(void)initAtPosition:(int)pos withPadding:(int)margin andDeveloperKey:(NSString *)key andDebuggable:(BOOL)isDebuggable andCustomMode:(BOOL)isCustomMode andRequestUUID:(NSString *)request_uuid;
+		[Static]
+		[Export ("initAtPosition:withPadding:andDeveloperKey:andDebuggable:andCustomMode:andRequestUUID:")]
+		void InitAtPosition (int pos, int margin, string key, bool isDebuggable, bool isCustomMode, string request_uuid);
+
+		// +(void)show;
+		[Static]
+		[Export ("show")]
+		void Show ();
+
+		// +(void)hide;
+		[Static]
+		[Export ("hide")]
+		void Hide ();
+
+		// +(void)destroy;
+		[Static]
+		[Export ("destroy")]
+		void Destroy ();
+
+		// +(void)updateLocationWithLatitude:(double)lat andLongitude:(double)lon andHorizontalAccuracy:(double)acc;
+		[Static]
+		[Export ("updateLocationWithLatitude:andLongitude:andHorizontalAccuracy:")]
+		void UpdateLocationWithLatitude (double lat, double lon, double acc);
+
+		// +(void)setAttributeDictionary:(NSMutableDictionary *)dict;
+		[Static]
+		[Export ("setAttributeDictionary:")]
+		void SetAttributeDictionary (NSMutableDictionary dict);
+	}
+}
+
---
+++ PollfishXamarinPhone/StructsAndEnums.cs
@@ -0,0 +1,429 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using ObjCRuntime;
+//using pollfish;
+
+namespace PollfishXamarinPhone
+{
+
+	public enum GenderValue : uint
+	{
+		Male,
+		Female,
+		Other
+	}
+
+	public enum RaceValue : uint
+	{
+		White,
+		Caucasian,
+		BlackAfrican,
+		HispanicLatino,
+		AsianPacificIslander,
+		ArabicMiddleEastern,
+		AmericanIndianAlaskaNativeNativeHawaiian,
+		OtherRace
+	}
+
+	public enum AgeGroupValue : uint
+	{
+		_1217,
+		_1824,
+		_2534,
+		_3544,
+		_4554,
+		_5564,
+		_65_plus,
+		OtherGroup
+	}
+
+	public enum AgeValue : uint
+	{
+		_12 = 0,
+		_13,
+		_14,
+		_15,
+		_16,
+		_17,
+		_18,
+		_19,
+		_20,
+		_21,
+		_22,
+		_23,
+		_24,
+		_25,
+		_26,
+		_27,
+		_28,
+		_29,
+		_30,
+		_31,
+		_32,
+		_33,
+		_34,
+		_35,
+		_36,
+		_37,
+		_38,
+		_39,
+		_40,
+		_41,
+		_42,
+		_43,
+		_44,
+		_45,
+		_46,
+		_47,
+		_48,
+		_49,
+		_50,
+		_51,
+		_52,
+		_53,
+		_54,
+		_55,
+		_56,
+		_57,
+		_58,
+		_59,
+		_60,
+		_61,
+		_62,
+		_63,
+		_64,
+		_65,
+		_66,
+		_67,
+		_68,
+		_69,
+		_70,
+		OtherAge
+	}
+
+	public enum MaritalStatusValue : uint
+	{
+		Annuled,
+		Divorced,
+		Interlocutory,
+		Legally,
+		Seperated,
+		Married,
+		Polygamous,
+		NeverMarried,
+		DomesticPartner,
+		Widowed,
+		OtherMarritalStatus
+	}
+
+	public enum ReligionValue : uint
+	{
+		Catholic,
+		Christian,
+		ProtestantChristian,
+		OrthodoxChristian,
+		AnglicanChristian,
+		Muslim,
+		Hindu,
+		Buddhist,
+		Bahai,
+		Secular,
+		NonReligious,
+		Agnostic,
+		Atheist,
+		OtherReligion
+	}
+
+	public enum PoliticalViewValue : uint
+	{
+		DemocraticParty,
+		RepublicanParty,
+		LibertarianParty,
+		GreenParty,
+		ConstitutionParty,
+		OtherPoliticalView
+	}
+
+	public enum YearOfBirthValue : uint
+	{
+		_1950,
+		_1951,
+		_1952,
+		_1953,
+		_1954,
+		_1955,
+		_1956,
+		_1957,
+		_1958,
+		_1959,
+		_1960,
+		_1961,
+		_1962,
+		_1963,
+		_1964,
+		_1965,
+		_1966,
+		_1967,
+		_1968,
+		_1969,
+		_1970,
+		_1971,
+		_1972,
+		_1973,
+		_1974,
+		_1975,
+		_1976,
+		_1977,
+		_1978,
+		_1979,
+		_1980,
+		_1981,
+		_1982,
+		_1983,
+		_1984,
+		_1985,
+		_1986,
+		_1987,
+		_1988,
+		_1989,
+		_1990,
+		_1991,
+		_1992,
+		_1993,
+		_1994,
+		_1995,
+		_1996,
+		_1997,
+		_1998,
+		_1999,
+		_2000,
+		_2001,
+		_2002,
+		_2003,
+		_2004,
+		_2005,
+		_2006,
+		_2007,
+		OtherYear
+	}
+
+	public enum NationalityValue : uint
+	{
+		Afghan,
+		Argentine,
+		Australian,
+		Belgian,
+		Bolivian,
+		Brazilian,
+		Comodian,
+		Cameroonian,
+		Canadian,
+		Chilean,
+		Chinese,
+		Colombian,
+		CostaRican,
+		Cypriot,
+		Danish,
+		Dominican,
+		Ecuadorian,
+		Egyptian,
+		Salvadorian,
+		English,
+		Esthonian,
+		Ethipian,
+		Finnish,
+		French,
+		German,
+		Ghanaian,
+		Greek,
+		Guatemalan,
+		Haitian,
+		Honduran,
+		Indonesian,
+		Iranina,
+		Irish,
+		Israeli,
+		Italian,
+		Japanese,
+		Jordanian,
+		Kenyan,
+		Korean,
+		Laotian,
+		Latvian,
+		Lithuanian,
+		Malaysian,
+		Mexican,
+		Moroccan,
+		Dutch,
+		NewZealander,
+		Nicaraguan,
+		Norwegian,
+		Panamanian,
+		Paraguayan,
+		Peruvian,
+		Filipino,
+		Polish,
+		Portuguese,
+		PuertoRican,
+		Romanian,
+		Russian,
+		Saudi,
+		Spanish,
+		Swedish,
+		Swiss,
+		Taiwanese,
+		Tajik,
+		Thai,
+		Turkish,
+		Ukrainian,
+		American,
+		Uruguanyan,
+		Venezuelan,
+		Vietnamese,
+		Welsh,
+		OtherNationality
+	}
+
+	public enum CountryValue : uint
+	{
+		Afganistan,
+		Argentina,
+		Australia,
+		Beligum,
+		Bolivia,
+		Brazil,
+		Cambodia,
+		Cameroon,
+		Canada,
+		Chile,
+		China,
+		Colombia,
+		CostaRica,
+		Cuba,
+		Cyprus,
+		Denmark,
+		DominicanRepublic,
+		RepublicEcuador,
+		Egypt,
+		ElSlavador,
+		England,
+		Estonia,
+		Ethiopia,
+		Finland,
+		France,
+		Germany,
+		Ghana,
+		Greece,
+		Guatemala,
+		Haiti,
+		Honduras,
+		Indonesia,
+		Iran,
+		Ireland,
+		Israel,
+		Italy,
+		Japan,
+		Jordan,
+		Kenya,
+		Korea,
+		Laos,
+		Latvia,
+		Lithuania,
+		Malaysia,
+		Mexico,
+		Morocco,
+		Netherlnads,
+		NewZealand,
+		Nicaragua,
+		Norway,
+		Panama,
+		Paraguay,
+		Peru,
+		Philippines,
+		Poland,
+		Portugal,
+		PuertoRico,
+		Romania,
+		Russia,
+		SaudiArabia,
+		Spain,
+		Sweden,
+		Switzerland,
+		Taiwan,
+		Tajikistan,
+		Thailand,
+		Turkey,
+		Ukraine,
+		UnitedStates,
+		Uruguay,
+		Venezuela,
+		Vietnam,
+		Wales,
+		OtherCountry
+	}
+
+	static class CFunctions
+	{
+		// extern NSString * GENDER (GENDER_VALUE gender);
+		[DllImport ("__Internal")]
+//		[Verify (PlatformInvoke)]
+		static extern string GENDER (GenderValue gender);
+
+		// extern NSString * RACE (RACE_VALUE race);
+		[DllImport ("__Internal")]
+//		[Verify (PlatformInvoke)]
+		static extern string RACE (RaceValue race);
+
+		// extern NSString * AGE_GROUP (AGE_GROUP_VALUE age_group);
+		[DllImport ("__Internal")]
+//		[Verify (PlatformInvoke)]
+		static extern string AGE_GROUP (AgeGroupValue age_group);
+
+		// extern NSString * AGE (AGE_VALUE age_value);
+		[DllImport ("__Internal")]
+//		[Verify (PlatformInvoke)]
+		static extern string AGE (AgeValue age_value);
+
+		// extern NSString * MARITAL_STATUS (MARITAL_STATUS_VALUE marital_status);
+		[DllImport ("__Internal")]
+//		[Verify (PlatformInvoke)]
+		static extern string MARITAL_STATUS (MaritalStatusValue marital_status);
+
+		// extern NSString * RELIGION (RELIGION_VALUE religion);
+		[DllImport ("__Internal")]
+//		[Verify (PlatformInvoke)]
+		static extern string RELIGION (ReligionValue religion);
+
+		// extern NSString * POLITICAL_VIEW (POLITICAL_VIEW_VALUE political_view);
+		[DllImport ("__Internal")]
+//		[Verify (PlatformInvoke)]
+		static extern string POLITICAL_VIEW (PoliticalViewValue political_view);
+
+		// extern NSString * YEAR_OF_BIRTH (YEAR_OF_BIRTH_VALUE year_of_birth);
+		[DllImport ("__Internal")]
+//		[Verify (PlatformInvoke)]
+		static extern string YEAR_OF_BIRTH (YearOfBirthValue year_of_birth);
+
+		// extern NSString * NATIONALITY (NATIONALITY_VALUE nationality);
+		[DllImport ("__Internal")]
+//		[Verify (PlatformInvoke)]
+		static extern string NATIONALITY (NationalityValue nationality);
+
+		// extern NSString * COUNTRY (COUNTRY_VALUE country);
+		[DllImport ("__Internal")]
+//		[Verify (PlatformInvoke)]
+		static extern string COUNTRY (CountryValue country);
+	}
+
+	[Native]
+	public enum PollfishPosition : long
+	{
+		TopLeft,
+		TopRight,
+		BottomLeft,
+		BottomRight,
+		MiddleLeft,
+		MiddleRight
+	}
+}
+
